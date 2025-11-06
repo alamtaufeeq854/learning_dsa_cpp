de50fdff3f77bde3778aabe331fab5b1cd59c783
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-11-06 19:45:56 +0530

    add finding length of a string file

---
+++ 05.strings/length_of_string.cpp
@@ -0,0 +1,17 @@
+// Finding Length of String
+#include <stdio.h>
+
+int main()
+{
+    char s[] = "Welcome";
+
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+    }
+
+    printf("Length Of String is %d\n", i);
+
+    return 0;
+}
