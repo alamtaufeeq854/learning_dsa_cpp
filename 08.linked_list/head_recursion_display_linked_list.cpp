e54740e4aa3114fa7c3ae11fcc04ea54ccb3e20e
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-11-14 08:14:36 +0530

    add head recursion display linked list file

---
+++ 08.linked_list/head_recursion_display_linked_list.cpp
@@ -0,0 +1,60 @@
+// Head Recursion Display Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+} *first = NULL;
+
+void create(int *A, int n)
+{
+    struct Node *t, *last;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+void recursionDisplay(struct Node *p)
+{
+    if (p != NULL)
+    {
+        recursionDisplay(p->next);
+        printf("%d ", p->data);
+    }
+}
+
+int main()
+{
+
+    int A[6] = {2, 9, 6, 7, 5, 1};
+
+    create(A, 6);
+
+    recursionDisplay(first);
+
+    return 0;
+}
