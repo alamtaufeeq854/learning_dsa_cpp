f261fd82fe0dfa6281856100f1fd769d4f446597
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-11-14 10:28:43 +0530

    add count length of linked list by recursion file

---
+++ 08.linked_list/count_length_of_linked_list_by_recursion.cpp
@@ -0,0 +1,61 @@
+// Count Length Of Linked List By Recursion
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+} *first = NULL;
+
+void create(int *A, int n)
+{
+    struct Node *t, *last;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int countByRecursion(struct Node *p)
+{
+
+    if (p)
+        return 1 + countByRecursion(p->next);
+
+    else
+        return 0;
+}
+
+int main()
+{
+
+    int A[] = {2, 9, 6, 7, 5, 1, 3, 8};
+
+    create(A, 8);
+
+    printf("\nLength is %d\n ", countByRecursion(first));
+
+    return 0;
+}
