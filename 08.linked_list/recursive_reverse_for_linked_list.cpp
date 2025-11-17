a846a7b70c1054888a3d020aac7b4f6107170345
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-11-17 19:19:36 +0530

    add recursive reverse for linked list file

---
+++ 08.linked_list/recursive_reverse_for_linked_list.cpp
@@ -0,0 +1,80 @@
+// Recursive Reverse For Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+} *first = NULL;
+
+void create(int *A, int n)
+{
+    struct Node *t, *last;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int count(struct Node *p)
+{
+    int l = 0;
+
+    while (p)
+    {
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+
+void reverse(struct Node *q, struct Node *p)
+{
+    if (p)
+    {
+        reverse(p, p->next);
+        p->next = q;
+    }
+
+    else
+        first = q;
+}
+
+int main()
+{
+
+    int A[6] = {2, 12, 16, 19, 25, 31};
+
+    create(A, 6);
+
+    display(first);
+
+    reverse(NULL, first);
+
+    printf("\n");
+    display(first);
+
+    return 0;
+}
