5851af762b0cbacc7419d7aeda936a03bd7ea599
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-11-23 13:27:47 +0530

    add doubly linked list file

---
+++ 08.linked_list/doubly_linked_list.cpp
@@ -0,0 +1,68 @@
+// Doubly Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+
+} *first = NULL;
+
+void create(int *A, int n)
+{
+    struct Node *t, *last;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->prev = NULL;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        last->next = t;
+        last = t;
+    }
+}
+
+int length(struct Node *p)
+{
+    int len = 0;
+
+    while (p != NULL)
+    {
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int main()
+{
+
+    int A[6] = {2, 9, 6, 7, 5, 1};
+
+    create(A, 6);
+
+    printf("\nLength is: %d\n", length(first));
+
+    display(first);
+
+    return 0;
+}
