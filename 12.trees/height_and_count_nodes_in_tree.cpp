688f742f046242b81f73dc62040a8a625d93f10d
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-12-07 21:57:44 +0530

    add height and count nodes in a tree file

---
+++ 12.trees/height_and_count_nodes_in_tree.cpp
@@ -0,0 +1,160 @@
+// Height And Count In Tree
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+};
+
+struct Queue
+{
+    int size;
+    int front;
+    int rear;
+    struct Node **Q;
+};
+
+void create(struct Queue *q, int size)
+{
+    q->size = size + 1;
+    q->front = q->rear = 0;
+    q->Q = (struct Node **)malloc(q->size * sizeof(struct Node *));
+}
+
+void enqueue(struct Queue *q, struct Node *x)
+{
+    if ((q->rear + 1) % q->size == q->front)
+        printf("Queue Is Full");
+
+    else
+    {
+        q->rear = (q->rear + 1) % q->size;
+        q->Q[q->rear] = x;
+    }
+}
+
+struct Node *dequeue(struct Queue *q)
+{
+    struct Node *x = NULL;
+
+    if (q->front == q->rear)
+        printf("Queue Is Empty");
+
+    else
+    {
+        q->front = (q->front + 1) % q->size;
+        x = q->Q[q->front];
+    }
+
+    return x;
+}
+
+int isEmpty(struct Queue q)
+{
+    return q.front == q.rear;
+}
+
+struct Node *root = NULL;
+
+void treeCreate()
+{
+    struct Node *p, *t;
+    int x;
+    struct Queue q;
+    create(&q, 100);
+
+    printf("Enter root value: ");
+    scanf("%d", &x);
+    root = (struct Node *)malloc(sizeof(struct Node));
+    root->data = x;
+    root->lchild = root->rchild = NULL;
+    enqueue(&q, root);
+
+    while (!isEmpty(q))
+    {
+        p = dequeue(&q);
+        printf("Enter left child of '%d': ", p->data);
+        scanf("%d", &x);
+        if (x != -1)
+        {
+            t = (struct Node *)malloc(sizeof(struct Node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->lchild = t;
+            enqueue(&q, t);
+        }
+        printf("Enter right child of '%d': ", p->data);
+        scanf("%d", &x);
+        if (x != -1)
+        {
+            t = (struct Node *)malloc(sizeof(struct
+                                             Node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->rchild = t;
+            enqueue(&q, t);
+        }
+    }
+}
+
+void preorder(struct Node *p)
+{
+    if (p)
+    {
+        printf("%d ", p->data);
+        preorder(p->lchild);
+        preorder(p->rchild);
+    }
+}
+
+void inorder(struct Node *p)
+{
+    if (p)
+    {
+        inorder(p->lchild);
+        printf("%d ", p->data);
+        inorder(p->rchild);
+    }
+}
+
+void postorder(struct Node *p)
+{
+    if (p)
+    {
+        postorder(p->lchild);
+        postorder(p->rchild);
+        printf("%d ", p->data);
+    }
+}
+
+int count(struct Node *root)
+{
+    if (root)
+        return count(root->lchild) + count(root->rchild) + 1;
+    return 0;
+}
+
+int height(struct Node *root)
+{
+    int x = 0, y = 0;
+    if (root == 0)
+        return 0;
+    x = height(root->lchild);
+    y = height(root->rchild);
+    if (x > y)
+        return x + 1;
+    else
+        return y + 1;
+}
+
+int main()
+{
+    treeCreate();
+
+    printf("Nodes: %d \n", count(root));
+    printf("Height: %d \n", height(root));
+    return 0;
+}
