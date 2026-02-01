77618c23f3b2794a9c0414f0609b274c075b48ce
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-02-01 06:47:44 +0530

    add permutation bounding function file

---
+++ 22.backtracking/permutation_bounding_function.cpp
@@ -0,0 +1,42 @@
+// Permutation Bounding Function
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define n 3
+
+char result[n + 1];
+bool sel[n] = {false, false, false};
+char str[] = "ABC";
+
+void permutation(int k)
+{
+    if (k == n)
+    {
+        result[k] = '\0';
+        printf("%s\n", result);
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (k == 0 || str[i] - result[k - 1] != 1)
+        {
+            if (sel[i] == false)
+            {
+                result[k] = str[i];
+                sel[i] = true;
+
+                permutation(k + 1);
+
+                sel[i] = false;
+            }
+        }
+    }
+}
+
+int main()
+{
+    permutation(0);
+    return 0;
+}
