e6dc4c6463db8582635bb988d237400b46f93b52
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-04 10:04:15 +0530

    add minimum element in linkedlist by recursion file

---
+++ 08.linked_list/minimum_element_in_linked_list_by_recursion.cpp
@@ -0,0 +1,53 @@
+// Minimum Element In Linked List By Recursion
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+} *first = NULL;
+
+void create(int *A, int n)
+{
+    struct Node *t, *last;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+int minRecursive(struct Node *p)
+{
+    if (p == NULL)
+        return INT_MAX;
+
+    int x = minRecursive(p->next);
+
+    return (p->data < x) ? p->data : x;
+}
+
+int main()
+{
+    int A[6] = {28, 9, 61, 7, 15, 51};
+
+    create(A, 6);
+
+    printf("\nMinimum element is: %d\n\n", minRecursive(first));
+
+    return 0;
+}
